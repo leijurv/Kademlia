0e69fe07144fe6975d1158f6a07051220335b3b1
Leif Jurvetson <leijurv@gmail.com>
2015-12-20 17:54:49 -0800

    1 sec

--- src/kademlia/DataStore.java
+++ src/kademlia/DataStore.java
@@ -197,7 +197,7 @@ public class DataStore {
             public void run() {
                 while (true) {
                     try {
-                        Thread.sleep(10000);
+                        Thread.sleep(1000);
                         if (shouldSave) {
                             doSave();
                         }
